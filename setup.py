7101f02bf2df4bc7f49ac9c0492f80c3a2173f44
oliov <oleksii.vorobiov@keemail.me>
2025-11-15 20:42:07 +0000

    chore: change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="personal_assistant",
-    version="0.1",
+    version="0.3",
     packages=["personal_assistant"],
     install_requires=[],
     entry_points={
